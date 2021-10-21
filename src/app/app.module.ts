8ad5a32f1429e7b78ddb9a1ad2b2098148c3c7b6
Paolo Capillo <capuz90@gmail.com>
2021-10-22 00:23:09 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {RouterModule} from "@angular/router";
+import {CoreModule} from "./core/core.module";
+import {createAction, createReducer, on, StoreModule} from "@ngrx/store";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,39 @@ import {RouterModule} from "@angular/router";
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    AppRoutingModule,
+    CoreModule,
+    StoreModule.forRoot(reducers, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true,
+        strictActionWithinNgZone: true,
+        strictActionTypeUniqueness: true,
+      },
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+const generateUniqueId = ():number => 1;
+
+export const addTodo = createAction(
+  '[Todo List] Add Todo',
+  (description: string) => ({ id: generateUniqueId(), description })
+);
+
+function reducers() {
+
+}
+let initialState;
+export const reducer = createReducer(
+  initialState,
+  on(addTodo, (state:any, { description }) => ({
+    ...state,
+    todos: [...state.todos, description],
+  }))
+);
