7f8390f479b1d219d47ae898b4b174709faa68a3
Paolo Capillo <capuz90@gmail.com>
2021-10-22 07:15:04 +0200

    global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {of} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'vercel';
 
-  constructor() {}
+  constructor() {
+    of(this.title)
+  }
 
 }
