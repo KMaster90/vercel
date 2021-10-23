c5933f1e09badd904bbda6a7a0e72ba973ac86dc
Paolo Capillo <capuz90@gmail.com>
2021-10-23 14:53:47 +0200

    store auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+import auth_config from '../../auth_config.json';
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    ...auth_config,
+    redirectUri: window.location.origin,
+  },
 };
